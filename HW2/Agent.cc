76d494fe98dd12ced528f274734cdf064a2dfc31
D1xonCider <bryans5@live.com>
2020-09-09 18:21:20 -0700

    Adding the HW2 code

--- HW2/Agent.cc
+++ HW2/Agent.cc
@@ -7,7 +7,11 @@ using namespace std;
 
 Agent::Agent ()
 {
-
+ x=1;
+ y=1;
+ facing=1;
+ hasGold=0;
+ shot=0;
 }
 
 Agent::~Agent ()
@@ -20,7 +24,7 @@ void Agent::Initialize ()
 
 }
 
-Action Agent::Process (Percept& percept)
+/* Action Agent::Process (Percept& percept)
 {
 	char c;
 	Action action;
@@ -49,10 +53,87 @@ Action Agent::Process (Percept& percept)
 		}
 	}
 	return action;
-}
+} */
 
 void Agent::GameOver (int score)
 {
 
 }
+//need to track direction facing, and x,y coordinates
+//need a random number generator, 0, 1, 2 for forward, left right
+//need a way to start automation (lets have input = a)
 
+Action Agent::Process (Percept& percept)//need to store the state outside of this.
+{
+	Action action;
+	//cout<<"x=:"<<x<<endl;
+	//cout<<"y=:"<<y<<endl; 
+	//wanted to be sure they initialized right
+	
+	int move= rand() %3;// 0 will be forward, 1 will be left, 2 will be right
+	
+	
+	if(percept.Glitter==true){
+		action=GRAB;
+		hasGold=1;
+	}
+	else if(x==1&&y==1&&hasGold==1){
+		action=CLIMB;
+	}
+	
+	//going to add a way to keep track of if i shot or not, this will be a 0 if not shot
+	else if(y==4&&facing==1&&shot==0){
+		action=SHOOT;
+		shot=1;
+	}
+	else if(x==4&&facing==0&&shot==0){
+		action=SHOOT;
+		shot=1;
+	}
+	//now we get to the movement part, this is where rand comes in
+	else if(move==0){
+		action=GOFORWARD;
+		if(facing==0){//up
+			if(y!=4){//can't go up more if at 4
+			y+=1;
+			}
+		}
+		else if(facing==1){//right
+			if(x!=4){
+			x+=1;
+			}
+		}
+		else if(facing==2){//down
+		if(y!=1){
+			y-=1;
+		}
+		}
+		else if(facing==3){//left
+			if(x!=1){
+			x-=1;
+			}
+		}
+	}
+	else if(move==1){//turn left
+		action=TURNLEFT;
+		if(facing==0){//dont want negatives
+			facing=3;
+		}
+		else{
+			facing-=1;
+		}
+	}
+	else if(move==1){//turn right
+		action=TURNRIGHT;
+		if(facing==3){//3 is cap
+			facing=0;
+		}
+		else{
+			facing+=1;
+		}
+	}
+	//remove this later, going to spit out what my stats are for debugging
+	//cout<<"X=: "<<x<<endl<<"Y=: "<<y<<endl<<"Facing: "<<facing<<endl;
+	
+	return action;
+}
--- HW2/Agent.h
+++ HW2/Agent.h
@@ -5,6 +5,7 @@
 
 #include "Action.h"
 #include "Percept.h"
+#include <stdlib.h>//so i have access to rand
 
 class Agent
 {
@@ -14,6 +15,16 @@ public:
 	void Initialize ();
 	Action Process (Percept& percept);
 	void GameOver (int score);
+	
+	int x;
+	int y;
+	int facing; //0, 1, 2, 3
+	/* 0
+	3    1
+	   2
+	*/
+	int hasGold; //0 for no gold, and 1 for gold
+	int shot;//0 for not shot, 1 for has shot
 };
 
 #endif // AGENT_H
